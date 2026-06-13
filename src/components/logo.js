ef3e0d479737102f3213f9246d9dc9ad6d9177cf
emontalban <mentulbenodarna@gmail.com>
2026-06-13 21:28:06 +0200

    añadido los componentes

--- src/components/logo.js
+++ src/components/logo.js
@@ -4,7 +4,7 @@ export default class Logo extends Component {
   render() {
     return (
       <div className='logo-main'>
-         <img src='/assets/ds_circle_logo.png'/>
+         <img src='/assets/ds_circle_logo.png' alt='logo'/>
       </div>
     );
   }
